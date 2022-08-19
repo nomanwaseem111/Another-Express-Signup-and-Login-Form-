e7c33aa0ea6af4bce07868e51b1007bf088bff11
Noman Waseem <nomanwaseem111@gmail.com>
2022-08-19 17:30:31 +0500

    today

---
+++ server.mjs
@@ -0,0 +1,128 @@
+import express from 'express'
+import cors from 'cors'
+import { nanoid } from 'nanoid'
+
+// console.log(nanoid());
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+const port = process.env.PORT || 3000
+
+let userBase = [];
+
+app.post('/signup', (req, res) => {
+  
+    let body = req.body;
+
+    if(!body.fname || !body.lname || !body.email || !body.password){
+
+        res.status(400).send(`required all fields, for example
+        
+          {
+            fname = "john",
+            lname = "doe",
+            email = "john@abc.com",
+            password = "1234",
+          }
+
+        
+        `)
+        return;
+    }
+
+    let isFound = false;
+
+    for(let i = 0; i < userBase.length; i++){
+   
+         if(userBase[i].email === body.email.toLowerCase()){
+            isFound = true
+            break;
+         }      
+
+    }
+
+    if(isFound){
+        res.status(400).send({ message : 'This Email id is Already Registered'})
+    }
+
+   
+    let newUsers = {
+        userId : nanoid(),
+        fname : body.fname,
+        lname : body.lname,
+        email : body.email.toLowerCase(),
+        password : body.password,
+    }
+
+
+   
+
+
+
+    userBase.push(newUsers);
+
+    res.status(201).send( {message : 'User is Created'})
+
+})
+
+
+
+
+app.post('/login', (req,res) => {
+
+    let body = req.body
+
+     if(!body.email || !body.password){
+
+        res.status(400).send(`required all fields,for example
+        
+          {
+               email : "john@abc@.com",
+               password : "1234"
+          }`)
+          return;
+     }
+
+
+      let isFound = false;
+
+   for(let i = 0; i < userBase.length; i++){
+
+      if(userBase[i].email === body.email){
+
+        isFound = true
+         if(userBase[i].password === body.password){
+
+             res.status(201).send(
+                {
+                     fname : body.fname,
+                     lname : body.lname,
+                     email : body.email.toLowerCase(),
+                     message : 'Login Successful'
+                })
+                return;
+
+         }else{
+            res.status(404).send({message : 'Please First Signup then Login'})
+            
+         }
+         return;
+        
+        }
+}
+
+
+
+if(!isFound){
+    res.status(404).send({message : 'User not Found'})
+    return;
+
+}
+
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
